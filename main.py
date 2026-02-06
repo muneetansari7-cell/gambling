26436783098db92620b7d73178edc3b7f036e8b5
Mohd Muneer <muneetansari7@gmail.com>
2026-02-06 11:59:27 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+print("------------Welcome to Zenin gambling------------")
+
+for i in range(5):
+    ash = int(input("enter a numbers between 1 to 50 :"))
+
+    num = random.randint(1,50)
+
+    if ash == num:
+        print("good")
+
+    elif ash>50:
+        print("choose vailid number")
+        exit()
+
+    else:
+        print("try again")
+
